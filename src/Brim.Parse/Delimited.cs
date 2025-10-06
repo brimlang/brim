231f3eb8b1f9a7eed71efc730463b976e8ed31ba
Charles Willis <5862883+trippwill@users.noreply.github.com>
2025-10-06 20:15:48 +0100

    refactor(parse): delete Delimited.cs legacy scaffold

--- src/Brim.Parse/Delimited.cs
+++
@@ -1,4 +0,0 @@
-// Legacy delimited helpers removed. Unified list parsing now lives in CommaList<T>.Parse.
-namespace Brim.Parse;
-
-internal static class Delimited { }
--- src/Brim.Parse/GlobalSuppressions.cs
+++ src/Brim.Parse/GlobalSuppressions.cs
@@ -2,7 +2,3 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
-
-[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:Brim.Parse.Delimited.ParseCommaListOpt``1(Brim.Parse.Parser,System.Func{Brim.Parse.Parser,System.ValueTuple{``0,System.Boolean}},Brim.Parse.RawKind[])~Brim.Parse.Collections.StructuralArray{``0}")]
-
